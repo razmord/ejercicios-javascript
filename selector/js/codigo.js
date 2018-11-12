60f9273abb19ec38862ce657d6a371f251157e04
Dana Martinez <32834298+razmord@users.noreply.github.com>
2018-11-12 09:48:31 +0100

    days with no internet

--- selector/js/codigo.js
+++ selector/js/codigo.js
@@ -30,12 +30,14 @@ function aplica(){
             opcionB=radio[i].value;
         }
     }
+    
     switch(opcionB){
     case "derecha": derecha.removeChild(creadoD); switch(opcionA){
         case "Parrafo": creadoD=document.createElement("p"); creadoD.appendChild(document.createTextNode(texto.value)); derecha.appendChild(creadoD); break;
         case "Imagen": creadoD=document.createElement("img"); creadoD.setAttribute("src", "imagenes/beast_hunter_draven.PNG"); derecha.appendChild(creadoD); break;
         case "Lista": creadoD=document.createElement("ul"); for(var i = 0;i<9;i++){aux=document.createElement("li"); aux.appendChild(document.createTextNode(i));creadoD.appendChild(aux);} derecha.appendChild(creadoD);  break;
         case "Enlace": creadoD=document.createElement("a"); creadoD.setAttribute("href", "imagenes/beast_hunter_draven.PNG"); creadoD.appendChild(document.createTextNode(texto.value)); derecha.appendChild(creadoD); break;
+        case "Animacion": creadoD=document.createElement("div"); creadoD.setAttribute("class","pantalla"); aux=document.createElement("div");aux.setAttribute("id","lute"); creadoD.appendChild(aux);aux=document.createElement("div");aux.setAttribute("id","lynn"); creadoD.appendChild(aux);derecha.appendChild(creadoD); break;
         default:break;
     }break;
     case "izquierda": izquierda.removeChild(creadoI); switch(opcionA){
@@ -43,6 +45,7 @@ function aplica(){
         case "Imagen": creadoI=document.createElement("img"); creadoI.setAttribute("src", "imagenes/beast_hunter_draven.PNG"); izquierda.appendChild(creadoI); break;
         case "Lista": creadoI=document.createElement("ul"); for(var i = 0;i<9;i++){aux=document.createElement("li"); aux.appendChild(document.createTextNode(i));creadoI.appendChild(aux);} izquierda.appendChild(creadoI);  break;
         case "Enlace": creadoI=document.createElement("a"); creadoI.setAttribute("href", "imagenes/beast_hunter_draven.PNG"); creadoI.appendChild(document.createTextNode(texto.value)); izquierda.appendChild(creadoI); break;
+        case "Animacion": creadoI=document.createElement("div"); creadoI.setAttribute("class","pantalla"); aux=document.createElement("div");aux.setAttribute("id","lute"); creadoI.appendChild(aux);aux=document.createElement("div");aux.setAttribute("id","lynn"); creadoI.appendChild(aux);izquierda.appendChild(creadoI); break;
         default:break;
     }break;
 
