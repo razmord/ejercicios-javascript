688ef74d346b9fb49d419ee8d845fa1dd8dcd696
Dana Martinez <32834298+razmord@users.noreply.github.com>
2018-11-19 13:25:09 +0100

    19N update

--- animada/js/codigo.js
+++ animada/js/codigo.js
@@ -4,6 +4,7 @@ var velo=document.querySelector("div.velo-principal");
 var boton=document.querySelector("div.boton");
 var texto=document.querySelector("div.boton > p");
 var icono=document.querySelector("div.boton > i");
+var escalas=document.querySelectorAll("div.menu > div");
 var abierto=false;
 boton.addEventListener('click', abreMenu,false);
 
@@ -11,17 +12,39 @@ function abreMenu(){
     if(abierto==false){
         menu.style.right="0";
         main.style.left="-300px";
+        texto.innerHTML=null;
+        icono.className=null;
+        setTimeout(ponVelo, 500);
         abierto=true;
     }
     else{
-        menu.style.right="-400px";
+        for ( i = 0; i < escalas.length; i++ ) {
+            escalas[i].style.transform= "scaleY(0)";
+          }
+        setTimeout(function(){ menu.style.right="-400px";
         main.style.left="0";
-        abierto=false;
+        texto.innerHTML=null;
+        icono.className=null;
+        velo.style.display="none";
+        setTimeout(aparecerBoton, 500);
+        abierto=false;},300);
     }
 }
 
+function ponVelo(){
+    velo.style.display="block";
+    texto.innerHTML="Cerrar";
+    icono.className="fas fa-arrow-alt-circle-left";
+    for ( i = 0; i < escalas.length; i++ ) {
+        escalas[i].style.transform= "scaleY(1)";
+      }
+    
+}
 
-
+function aparecerBoton(){
+    texto.innerHTML="Menú";
+    icono.className="fas fa-arrow-alt-circle-right";
+}
 
 
 
