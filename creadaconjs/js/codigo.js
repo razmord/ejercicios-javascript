5c6467d69ca1d7aea2eb36022661949e12f89fba
Dana Martinez <32834298+razmord@users.noreply.github.com>
2018-11-20 09:44:40 +0100

    paginacreadaconjs

---
+++ creadaconjs/js/codigo.js
@@ -0,0 +1,17 @@
+var body=document.querySelector("body");
+var nav=document.createElement("nav");
+body.appendChild(nav);
+var div1=document.createElement("div");
+nav.appendChild(div1);
+var lista=document.createElement("ul");
+div1.appendChild(lista);
+var elelista=new Array(3);
+for(var i=0; i<elelista.length;i++){
+    elelista[i]=document.createElement("li");
+    lista.appendChild(elelista[i]);
+    switch(i){
+        case 0: elelista[i].innerHTML="UNO"; break;
+        case 1: elelista[i].innerHTML="DOS"; break;
+        case 2: elelista[i].innerHTML="TRES"; break;
+    }
+}
